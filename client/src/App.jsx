1af1879884b615bd19f2e564ab12a5891b6e8424
Prathmesh Mathur <79357254+prthm412@users.noreply.github.com>
2025-04-02 16:41:15 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind + Vite is Working!</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Student Task Manager (Vite + Tailwind)</h1>
     </div>
   );
 }
 
+export default App;
 
-export default App
