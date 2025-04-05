eb362b661c3b9ab03634bd551c52be04c3a8249e
Prathmesh Mathur <79357254+prthm412@users.noreply.github.com>
2025-04-05 14:58:37 +0530

    Add Sequelize User model and sync with DB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import sequelize from "./config/db.js";
+import User from "./models/User.js"
 
 dotenv.config();
 
@@ -15,6 +17,16 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try{
+        await sequelize.authenticate();
+        console.log("Connected to PostgreSQL DB");
+
+        await sequelize.sync();
+        console.log(" - Models synchronized with the database");
+    } catch (error) {
+        console.error("Unable to connect to DB: ", error);
+    }
+
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+export default User;
